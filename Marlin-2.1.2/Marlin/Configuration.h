dab96b3fd9d4f2a0d68f309d99b713d409749e88
Sergio Gugliandolo <gugliandolosergio@gmail.com>
2023-05-07 16:14:29 +0200

    minor

--- Marlin-2.1.2/Marlin/Configuration.h
+++ Marlin-2.1.2/Marlin/Configuration.h
@@ -65,7 +65,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Sergio Gugliandolo)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "Sergio Gugliandolo" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
